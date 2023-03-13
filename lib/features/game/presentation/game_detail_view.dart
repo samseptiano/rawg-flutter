0f177afe7d7e08d9a3569f6c678dcfe72980ffd0
samuel septiano <sammuel.septiano@gmail.com>
2023-03-13 20:04:56 +0700

    add game list detail
    add game list detail

---
+++ lib/features/game/presentation/game_detail_view.dart
@@ -0,0 +1,96 @@
+import 'package:bloc_tutorial/assets/app_color.dart';
+import 'package:bloc_tutorial/assets/app_string.dart';
+import 'package:bloc_tutorial/features/game/model/game_response.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class GameDetailView extends StatefulWidget {
+  late Result resultDetail;
+
+  GameDetailView({required this.resultDetail});
+
+  @override
+  State<GameDetailView> createState() => _GameDetailViewState();
+}
+
+class _GameDetailViewState extends State<GameDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppString().AppName),
+        actions: <Widget>[
+          SizedBox(
+              height: 38.0,
+              width: 38.0,
+              child: new IconButton(
+                icon: new Image.asset('images/icon_image.png'),
+                onPressed: () {
+                  // do something
+                },
+              ))
+        ],
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              child: Image.network(
+                widget.resultDetail.backgroundImage.toString(),
+                fit: BoxFit.fill,
+                height: double.infinity,
+                width: double.infinity,
+                alignment: Alignment.center,
+                loadingBuilder: (BuildContext context, Widget child,
+                    ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  );
+                },
+                color: Colors.black45,
+                colorBlendMode: BlendMode.darken,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Container(
+                  child: Text(
+                widget.resultDetail.name.toString(),
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: AppColors.black,
+                    shadows: [
+                      Shadow(
+                          // bottomLeft
+                          offset: Offset(-1.5, -1.5),
+                          color: AppColors.primaryTheme),
+                      Shadow(
+                          // bottomRight
+                          offset: Offset(1.5, -1.5),
+                          color: AppColors.primaryTheme),
+                      Shadow(
+                          // topRight
+                          offset: Offset(1.5, 1.5),
+                          color: AppColors.black),
+                      Shadow(
+                          // topLeft
+                          offset: Offset(-1.5, 1.5),
+                          color: AppColors.primaryTheme),
+                    ]),
+              )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
